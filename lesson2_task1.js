171085f97e90f656d090199511f0b61cb014a52e
«Rovshan» <r.mekhtiev@mail.ru>
2024-09-18 01:01:17 +0200

    added lesson2_task1.js and lesson2_task2.js

---
+++ lesson2_task1.js
@@ -0,0 +1,4 @@
+const salary = 1000;
+const grade = salary >= 100 ? "middle" : "junior";
+console.log(salary);
+console.log(grade);
---
+++ lesson2_task2.js
@@ -0,0 +1,17 @@
+const minAge = 18;
+const maxAge = 60;
+const age = 61;
+
+if (typeof age !== "number") {
+  console.log("Incorrect data type");
+} else if (age < minAge) {
+  console.log(
+    "You dont have access cause your age is " + age + " Its less then " + minAge
+  );
+} else if (maxAge > age >= minAge) {
+  console.log("welcome !");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel");
+} else {
+  console.log("Technical work");
+}
